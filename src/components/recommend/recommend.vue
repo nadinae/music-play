70f6b3d7b562cda3f5086e64e084f9012b2b864f
nadinae <itnadinae@sina.com>
2019-05-10 00:06:33 +0800

    css

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,19 +1,35 @@
 <template>
-    <div>
-      <div>
-        <Scroll>
-          这是一段文字
-        </Scroll>
-      </div>
+    <div class="recommend">
+        <div class="slider-content">
+            <mt-swipe v-if="recommends.length" :auto="4000" class="slider flex">
+                <mt-swipe-item v-for="(item,index) in recommends" class="slider-item" :key="index">
+                    <a :href="item.linkUrl">
+                        <img :src="item.picUrl">
+                    </a>
+                </mt-swipe-item>
+            </mt-swipe>
+        </div>
+        <div class="recommend-list">
+            <h1 class="list-title">热门歌单推荐</h1>
+            <ul></ul>
+        </div>
     </div>
 </template>
 <script>
-import Scroll from 'base/scroll/scroll'
+// import Slider from 'base/slider/slider'
 import {getRecommend} from 'api/recommend'
 import {ERR_OK} from 'api/config'
+import { Swipe, SwipeItem } from 'mint-ui';
+
 export default {
+    data(){
+        return {
+            recommends:[]
+        }
+    },
     components:{
-      Scroll,
+    'mt-swipe':Swipe,
+    'mt-swipe-item':SwipeItem
     },
     created(){
         this._getRecommend()
@@ -22,15 +38,34 @@ export default {
         _getRecommend(){
             getRecommend().then((res)=>{
                 if(res.code === ERR_OK){
-                    console.log(res.data.slider)
+                    this.recommends = res.data.slider;
                 }
             })
         }
     }
 }
 </script>
-<style scoped>
+<style>
 div{
     color:#fff;
+}
+.slider{
+    width:100%;
+    height:150px;
+}
+.mint-swipe-items-wrap{
+    width:100%;
+}
+.slider-item{
+    flex-shrink:0;
+}
+.slider-item a{
+  display:block;
+  width:100%;
+  overflow:hidden;
+}
+.slider-item img{
+  display:block;
+  width:100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import './common/js/px2rem.js'
 import './common/css/common.css'
 
 Vue.config.productionTip = false
-
+Vue.use(MintUI)
 fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
