e1cf5a61c4d2fede1a5c5c4f2ddc3722e8bf2902
nadinae <itnadinae@sina.com>
2019-06-24 22:11:02 +0800

    css

--- src/common/js/cache.js
+++ src/common/js/cache.js
@@ -1,9 +1,9 @@
-// import storage from 'good-storage'
+import storage from 'good-storage'
 
-// const SEARCH_KEY = '_search_';
-// const SEARCH_MAX_LENGTH = 15;
-// export function saveSearch(query){
+const SEARCH_KEY = '_search_';
+const SEARCH_MAX_LENGTH = 15;
+export function saveSearch(query){
 
 
 
-// }
+}
